9a7674a4ab1150bdb990f358e5b6c801903d6b45
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-01-29 12:31:50 -0500

    question4.py

--- question4.py
+++ question4.py
@@ -1,24 +1,32 @@
 def question4_1():
-    # answer = ['string', 'string']
+    answer = ['nominal', 'nominal']
     return answer
 
 def question4_2():
+    answer = ['ratio', 'ordinal']
     return answer
 
 def question4_3():
+    answer = ['ratio', 'ordinal']
     return answer 
 
 def question4_4():
+    answer = ['ratio', 'ordinal']
     return answer
 
 def question4_5():
+    answer = ['ratio', 'ratio']
     return answer
 
 def question4_6():
+    answer = ['ratio', 'ordinal']
     return answer
 
 def question4_7():
+    answer = ['ratio', 'ratio']
     return answer
 
 def question4_8():
+    answer = ['ratio', 'ratio']
     return answer
+
