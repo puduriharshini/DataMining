ecd923318ca6028fd9c20a9bc75b21811d53b097
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-01-29 18:38:40 -0500

    question10.py

--- question10.py
+++ question10.py
@@ -1,32 +1,41 @@
 def question10_1():
-    """
-    answer: python bool (True or False)
-    """
+    
+    answer: bool = False
+    #count data might sometimes have noise because of sensor inaccuracies
     return answer
 
 def question10_2():
+    answer: bool = True
     return answer
 
 def question10_3():
+    answer: bool = True
     return answer
 
 def question10_4():
+    answer: bool = False #data = true signal + noise. Outliers are part of the data.
     return answer
 
 def question10_5():
+    answer: bool = False #outlier is a data point that differs from most of the data in the given set, whereas noise is unwanted/undesirable part of data.
     return answer
 
 def question10_6():
+    answer: bool = False
     return answer
 
 def question10_7():
+    answer: bool = True
     return answer
 
 def question10_8():
+    answer: bool = False
     return answer
 
 def question10_9():
+    answer: bool = False
     return answer
 
 def question10_10():
+    answer: bool = False
     return answer
