f4e8c78d54a5801fd5d81d64432d51bf0cac687a
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-01-29 14:22:10 -0500

    question9.py

--- question9.py
+++ question9.py
@@ -1,18 +1,23 @@
 def question9_1():
-    """
-    answer = string'
-    """
+    
+    answer = 'SMC'
+    
     return answer
 
 def question9_2():
+    answer = 'Jaccard'
     return answer
 
 def question9_3():
+    answer = 'Correlation Coefficient'
     return answer 
 
 def question9_4():
+    answer = 'Euclidean Distance'
     return answer 
 
 def question9_5():
+    answer = 'Cosine Similarity'
     return answer 
+
 
