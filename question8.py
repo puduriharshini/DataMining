46a5203848182133d01df8faaec82947e130b539
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-01-29 20:42:54 -0500

    question8.py

--- question8.py
+++ question8.py
@@ -1,17 +1,17 @@
 def question8_1():
    
-    answer= ['x1', 'x2']
+    answer = ['x1', 'x2']
     
     return answer
 
 def question8_2():
-    answer= 'equally similar'
+    answer = 'equally similar'
     return answer
 
 def question8_3():
-    answer= 'equally similar'
+    answer = 'equally similar'
     return answer 
 
 def question8_4():
-    answer= ['x2','y3']
+    answer = ['x2','y3']
     return answer 
