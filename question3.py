e34097fa89a9ebc3e73c048b50447e8027bcf6ed
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-01-29 19:25:19 -0500

    question3.py

--- question3.py
+++ question3.py
@@ -1,5 +1,5 @@
 def question3_1():
-    answer = ['continuous, 'quantitative', 'ratio']
+    answer = ['continous', 'quantitative', 'interval']
     return answer
 
 def question3_2():
@@ -7,11 +7,11 @@ def question3_2():
     return answer
 
 def question3_3():
-    answer = ['discrete', 'qualitative', 'ordinal']
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer 
 
 def question3_4():
-    answer = ['continuous, 'quantitative', 'ratio']
+    answer = ['continous', 'quantitative', 'ratio']
     return answer
 
 def question3_5():
@@ -19,13 +19,12 @@ def question3_5():
     return answer
 
 def question3_6():
-    answer = ['discrete', 'qualitative', 'ordinal']
+    answer = ['continous', 'quantitative', 'ratio']
     return answer
 
 def question3_7():
-    answer = ['discrete', 'quantitative', 'ratio']
+    answer = ['continous', 'quantitative', 'ratio']
     return answer
 
 def question3_8():
     answer = ['discrete', 'qualitative', 'ordinal']
-    return answer
