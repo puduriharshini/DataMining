6792be5af554ad75325792cb1cbca0da283168d6
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-01-29 20:45:13 -0500

    question7.py

--- question7.py
+++ question7.py
@@ -7,5 +7,5 @@ def question7_2():
     return answer
 
 def question7_3():
-    answer = [(0, 2), (2, 3), (3,5), (5, 'infinity')]
+    answer = [(0., 2.), (2., 3.), (3.,5.), (5., 'infinity')]
     return answer
