c82a2e17b7b97cb9c7b7d5eb3af2fdf1a7063d3a
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-01-30 21:33:48 -0500

    question6.py

--- question6.py
+++ question6.py
@@ -1,15 +1,15 @@
 def question6_1():
     
     answer = {
-        'equal_width': ['no change', 9],
-        'equal_frequency': ['no change', 9]
+        'equal_width': ['no change', 3],
+        'equal_frequency': ['no change', 3]
     }
     
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['no change', 9],
+        'equal_width': ['no change', 7],
         'equal_frequency': ['no change', 9]
     }
     return answer
